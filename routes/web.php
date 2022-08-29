d4bd54b3c2841e79ca362f29411af40afbdb0454
André <andregds85@gmail.com>
2022-08-29 15:40:20 -0300

    melhorando um pouco o layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('vue');
+});
+
+
+/*
+Route::get('/', function () {
     return view('welcome');
 });
+
+*/
